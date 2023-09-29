6f645020738e4b801a278af762cc4dc38e69818c
Bhupender2 <b.mehtaa2021@gmail.com>
2023-09-29 16:54:16 +0530

    script file added

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JAVASCRIPT IS FUN");
+// 10 + 20 + 20;
+// console.log(10 + 20 + 20);
+
+// Art of naming the variable
+
+let firstName = "Bhupender"; // giving camelCase naming is a good practice
+let lastName = "Mehta";
+
+console.log(firstName); //giving semicolons after every line is a good coding practice
+
+let first_name = "Bhupender"; //this type of naming conventions is very popular in ruby
+
+//you should not use reserved keywords like new and functions it will throw errors
+
+// your naming convention should be descriptive and jisse humein pta chal jaaye ki hum kiski baat karahe h it will help in writing cleaner code
+
+let myFirstJob = "Programmer";
+let mySecondJob = "Developer";
+console.log(myFirstJob);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+//now giving some value to year
+year = 1998;
+
+console.log(typeof year);
+
+console.log("bhupi" + " is" + " hot");
+console.log(2 ** 3);
+console.log(27 > 290);
