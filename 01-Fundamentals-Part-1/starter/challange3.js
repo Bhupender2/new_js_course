f458f3d72bc5248c3b127d1c2a1efde3fedc54f9
Bhupender2 <b.mehtaa2021@gmail.com>
2023-10-07 21:00:10 +0530

    switch statement

---
+++ 01-Fundamentals-Part-1/starter/challange3.js
@@ -0,0 +1,51 @@
+const scoreDolphin = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 100 + 110) / 3;
+console.log(scoreDolphin, scoreKoalas);
+
+if (scoreDolphin > scoreKoalas && scoreDolphin >= 100) {
+  console.log("Dolphin wins the trophy 🏆");
+} else if (scoreDolphin < scoreKoalas && scoreKoalas >= 100) {
+  console.log("koalas wins the trophy 🏆");
+} else if (
+  scoreDolphin === scoreKoalas &&
+  scoreDolphin >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("its a tie 🏆 ");
+} else {
+  console.log("no ones wins the trophy 😢 ");
+}
+
+//switch case
+
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("i would plan the course structure");
+    console.log("i would go to coding meetup");
+    break;
+
+  case "tuesday":
+    console.log("i would prepare theory videos");
+    break;
+
+  case "wednesday":
+  case "thursday":
+    console.log("i would write code examples");
+    break;
+
+  case "friday":
+    console.log("i would record coding videos");
+    break;
+
+  case "saturday":
+  case "sunday":
+    console.log("i will enjoy my weekend :D");
+    break;
+
+  default:
+    console.log("its not a valid day !");
+}
+
+//now we will do
