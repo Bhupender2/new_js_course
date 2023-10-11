4314c07c4a617c32852b6ef4461401e1c6f47c63
Bhupender2 <b.mehtaa2021@gmail.com>
2023-10-11 20:15:20 +0530

    function declaration and expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,3 +9,27 @@ function logger() {
 logger();
 logger();
 logger();
+
+function foodProcessor(apple, oranges) {
+  const juice = `juice with ${apple} apples and ${oranges} oranges`;
+  return juice;
+}
+const appleJuice = foodProcessor(5, 0);
+console.log(appleJuice);
+
+//function declaration and expressions
+
+// function declaration and you can call them before even defining it which is basically not a good practice
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1998);
+console.log(age1);
+
+//function expressions and you can not call them before defining it
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1996);
+console.log(age1, age2);
