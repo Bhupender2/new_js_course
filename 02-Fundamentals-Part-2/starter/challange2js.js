152e9973f6198ed95d25d9a416a1aba7cd27aea0
Bhupender2 <b.mehtaa2021@gmail.com>
2023-10-20 14:23:53 +0530

    coding challange 4 done

--- 02-Fundamentals-Part-2/starter/challange2js.js
+++ 02-Fundamentals-Part-2/starter/challange2js.js
@@ -40,7 +40,7 @@ const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]; //creating an arra
 const tips = [];
 const totals = []; // creating an empty array of totals and tips
 
-const calcTip = function () {
+const calcTip = function (bills) {
   return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
 };
 
@@ -49,3 +49,28 @@ for (let i = 0; i < bills.length; i++) {
   totals.push(bills[i] + calcTip(bills[i]));
 }
 console.log(tips, totals);
+
+//coding question -4 javascript fundamental part 2
+
+// const arr = [110, 220, 330, 1, 2, 5, 9];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+// }
+// console.log(sum);
+// let calcAverage = sum / arr.length;
+// console.log(calcAverage);
+// this is a hardcoded version where array
+
+//this is more appropriate method where we take arr as an argument so that we can put any big or small array as we want
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage([10, 20, 30, 30]));
+console.log(calcAverage(bills));
