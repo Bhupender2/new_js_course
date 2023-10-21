32d9cba8096bc2d3a332d5084757120c62c971c3
Bhupender2 <b.mehtaa2021@gmail.com>
2023-10-21 20:57:21 +0530

    how to approach a question

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,2 +1,38 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+//what is temp amplitude Answer: difference between highest and the lowest temp
+
+// understanding the problem
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// real question is how to compute the min and max temperture in array
+// what is sensor error look like
+
+// breaking up into sub-problems
+//1. how to ignore errors
+//2. find the max and min value
+//3. sub min from max and return it
+
+// create a function where we take array as an argument so we make a temp array
+
+const calcTempAmpltitude = function (temps1, temps2) {
+  const temps = temps1.concat(temps2);
+  // Manager said we now recieve two array as an argument here
+  // so there is only one problem here which is how to merge two arrays
+  // we will merge the do array using concat() method
+  console.log(temps.length);
+  let max = temps[0]; // first we need to initialise the min and  max values atleast na
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currTemp = temps[i];
+
+    if (typeof currTemp !== "number") continue; // if the currTemp is different than no so exit from the current iteration and move on the next one
+    if (currTemp > max) max = currTemp;
+    if (currTemp < min) min = currTemp;
+  }
+  console.log(max, min);
+  return max - min; //return an temperature amplitude which is the difference between max and min value
+};
+console.log(calcTempAmpltitude([1, 2, 3, 4, 5, 88], [2, 9, 4, 6, 90]));
