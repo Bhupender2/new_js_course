a43384a3251d3d9e1bc0c0997ec2d4f9e2a726bd
Bhupender2 <b.mehtaa2021@gmail.com>
2023-11-02 18:01:54 +0530

    guess my number game modification started

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// document.querySelector('.message').textContent = 'start guessing now bitch';
+
+// //now we want to add some input value using javascript
+
+// document.querySelector('.guess').value = 10;
+
+// //now we are manipulating the score
+// document.querySelector('.score').textContent = 99;
+
+// // whatever we have done till now here is called dom manipuation because we are actually manipulating dom using javascript
+
+//NOW WE ARE MAKING THE REAL GAME
+// we want to do something on clicking the botton
+// after selecting the check class ...we simply use addeventlistner . basically addeventlistener just watches for event to happen and then it trigerr the event handler to execute the instruction ..and event handler is the code which execute when the event occurs
+document.querySelector('.check').addEventListener('click');
