6198c17f380848e4f93840a75bf5e7a402cce41b
Bhupender2 <b.mehtaa2021@gmail.com>
2023-11-18 01:59:58 +0530

    coding challange completed

--- 09-Data-Structures-Operators/starter/codingChallange.js
+++ 09-Data-Structures-Operators/starter/codingChallange.js
@@ -59,3 +59,41 @@ console.log(allPlayers);
 
 const players1Final = ['Thiago', 'cautinho', 'perisic', ...players1];
 console.log(players1Final);
+
+//question 5-- we can to change the names of the odds object variabe
+
+// const { team1, team2, x: draw } = game.odds;
+// console.log(team1, draw, team2);
+
+//----- OR WE CAN DO LIKE THIS------
+
+const {
+  odds: { team1, team2, x: draw },
+} = game;
+console.log(team1, draw, team2);
+
+// Question 6
+const printGoals = function (...players) {
+  //we use rest operator to make an array here
+  console.log(players);
+  console.log(
+    `total goals scored are ${players.length} and name of the players are ${players[0]}, ${players[1]}, ${players[2]}, ${players[3]} `
+  );
+};
+
+printGoals(...game.scored); // we use spread operator to expand all the elements out of the array with a comma
+
+//question 7 team with the lower odd is more likely to win
+
+team1 < team2 && console.log('Team 1 is more likely to win ');
+
+// regular doubt
+
+const rest1 = {
+  name: 'chapri',
+  numGuests: 20,
+};
+const rest2 = {
+  owner: 'bhupender',
+  name: 'pizza la pizza',
+};
