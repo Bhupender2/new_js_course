19f76a12b5ca80f6ba05e72c0004ed1164a97c42
Bhupender2 <b.mehtaa2021@gmail.com>
2023-11-28 04:09:09 +0530

    string methids includes

--- 09-Data-Structures-Operators/starter/codingChallange1.js
+++ 09-Data-Structures-Operators/starter/codingChallange1.js
@@ -55,3 +55,46 @@ for (const [keys, values] of gameEvents) {
   const half = keys >= 45 ? 'Second' : 'First';
   console.log(`[${half} Half] ${keys}:${values}`);
 }
+
+//string airplane question
+
+const middleSeat = function (seat) {
+  const s = seat.slice(-1);
+  // b and e are middle seat
+  s === 'B' || s === 'E'
+    ? console.log('you are sitting in the middle seat')
+    : console.log('you are lucky today');
+};
+
+middleSeat('11B');
+
+// seeing if passengers are allowed or not in planes
+
+const checkBaggage = function (items) {
+  const baggageItem = items.toLowerCase(); // need to put everything first to lowercase otherwise there will be very variation like KNIFE , Knife,kNife, kniFE  which is very impractical so first put everything to lowwercase and then do all the work
+  // baggageItem.includes('knife').includes('gun')     ------it returns a boolean not a string so chaining is not possible
+  baggageItem.includes('knife') || baggageItem.includes('gun')
+    ? console.log('you are not allowed')
+    : console.log('welcome abord');
+};
+
+checkBaggage('i have a Knife');
+checkBaggage('i have a gun');
+checkBaggage('i have a knife and a gun');
+checkBaggage('i have a candy');
+
+//comparing emails
+
+const compEmail = function (email, email2) {
+  const needToChangeEmail = email2.toLowerCase().trim();
+
+  needToChangeEmail === email
+    ? console.log(`both are same email`)
+    : console.log('both are different email');
+};
+
+compEmail('hello@jonas.io', '  Hello@Jonas.Io  \n');
+
+const announcement = 'all the passangers comes to door 23, immediately door 23';
+
+console.log(announcement.replace('door', 'gate'));
