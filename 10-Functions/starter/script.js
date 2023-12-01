b2d36515a382a44cdd16c35693035f1ddd736bd9
Bhupender2 <b.mehtaa2021@gmail.com>
2023-12-02 01:58:20 +0530

    returning and taking functions as arguments

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// function 1
+
+const upperWord = function (str) {
+  const [firstWord, ...others] = str.split(' ');
+  return [firstWord.toUpperCase(), ...others].join(' ');
+};
+
+// function 2
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const transformer = function (str, fn) {
+  // this is a H.O function coz its take a function as an argument
+  console.log(`string that is going to transformed: ${str}`);
+  console.log(`transformed string : ${fn(str)}`);
+  console.log(`transformed by : ${fn.name}`); // name is a function property
+};
+
+transformer('javascript is the best language in the world', oneWord);
+transformer('javascript is the best language in the world', upperWord);
+
+//writing the function returning function with using arrow function
+
+const greet = greeting => name => {
+  console.log(`${greeting} ${name}`);
+};
+
+const greeterHey = greet('hey');
+greeterHey('jonas');
+greeterHey('bhupender');
