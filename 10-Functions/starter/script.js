fd4ee905bf2db7806a8820e8b8cae52d238ea3bc
Bhupender2 <b.mehtaa2021@gmail.com>
2023-12-03 03:17:02 +0530

    all dome

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,50 +1,50 @@
 'use strict';
 
-// function 1
+// // function 1
 
-const upperWord = function (str) {
-  const [firstWord, ...others] = str.split(' ');
-  return [firstWord.toUpperCase(), ...others].join(' ');
-};
+// const upperWord = function (str) {
+//   const [firstWord, ...others] = str.split(' ');
+//   return [firstWord.toUpperCase(), ...others].join(' ');
+// };
 
-// function 2
+// // function 2
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const transformer = function (str, fn) {
-  // this is a H.O function coz its take a function as an argument
-  console.log(`string that is going to transformed: ${str}`);
-  console.log(`transformed string : ${fn(str)}`);
-  console.log(`transformed by : ${fn.name}`); // name is a function property
-};
+// const transformer = function (str, fn) {
+//   // this is a H.O function coz its take a function as an argument
+//   console.log(`string that is going to transformed: ${str}`);
+//   console.log(`transformed string : ${fn(str)}`);
+//   console.log(`transformed by : ${fn.name}`); // name is a function property
+// };
 
-transformer('javascript is the best language in the world', oneWord);
-transformer('javascript is the best language in the world', upperWord);
+// transformer('javascript is the best language in the world', oneWord);
+// transformer('javascript is the best language in the world', upperWord);
 
-//writing the function returning function with using arrow function
+// //writing the function returning function with using arrow function
 
-const greet = greeting => name => {
-  console.log(`${greeting} ${name}`);
-};
+// const greet = greeting => name => {
+//   console.log(`${greeting} ${name}`);
+// };
 
-const greeterHey = greet('hey');
-greeterHey('jonas');
-greeterHey('bhupender');
+// const greeterHey = greet('hey');
+// greeterHey('jonas');
+// greeterHey('bhupender');
 
 // the call and the apply method
 
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
-  booking: [],
+  bookings: [],
   // book : function(){} ----we can right this but we use es6 method shorthand in object
   book(flightNum, name) {
     console.log(
       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}` // this keyword will point to the object whom the method is called --book method in called on which method
     );
-    this.booking.push({
+    this.bookings.push({
       flight: ` ${this.iataCode}${flightNum}`,
       name,
     });
@@ -59,7 +59,7 @@ lufthansa.book(999, 'kritika Mehta');
 const eurowings = {
   airline: 'Eurowings',
   iataCode: 'EU',
-  booking: [],
+  bookings: [],
   //simply copying the book method here is a bad practice so we'll not do it so we will just store that method in an external function so that we can use it again and again
 };
 
@@ -67,12 +67,12 @@ const book = lufthansa.book; // creating an external function here and store the
 
 // book(23, 'sara williams') -- now book is a regular function so here this keyword will be undefined atleast in STRICT MODE --so now to tell which object this keyword should point or we can say how this keyword should look like it will be possible with the help of three methods (call , apply , bind )
 
-book.call('eurowings', 23, 'sarah williams'); // first argument is exactly where we want this keyword to point to and then the rest of the arguments-- here we call the call method which then call the Book function with this keyword set to EUROWING ....
+book.call(eurowings, 23, 'sarah williams'); // first argument is exactly where we want this keyword to point to and then the rest of the arguments-- here we call the call method which then call the Book function with this keyword set to EUROWING ....
 console.log(eurowings);
 
 // call method allow to set this keyword explicitly
 
-book.call('lufthansa', 77, 'bhilli bi');
+book.call(lufthansa, 77, 'bhilli bi');
 console.log(lufthansa);
 
 // apply method similar to call but it will take this keyword as its first arguments and the rest as an array of arguments(data)
